aa4c961bd0c2ed6fe3842c50a4d3b2eb62007280
ajones-programming <ajones.programming@gmail.com>
2024-09-26 07:19:35 +0100

    misc

--- src/Components/kitchen.tsx
+++ src/Components/kitchen.tsx
@@ -313,9 +313,9 @@ export default function Kitchen() {
     middleList = (
       <div>
         <div dir="horizontal" style={style1}>
-          <img alt={leftEmojiData?.name} id={leftEmoji.id()} src={leftEmojiURL} width="100px" height="100px" style={imgStyle}/>
+          <img alt={leftEmojiData?.twemoji_name} id={leftEmoji.id()} src={leftEmojiURL} width="100px" height="100px" style={imgStyle}/>
           +
-          <img alt={rightEmojiData?.name} id={rightEmoji.id()} src={rightEmojiURL} width="100px" height="100px" style={imgStyle}/>
+          <img alt={rightEmojiData?.twemoji_name} id={rightEmoji.id()} src={rightEmojiURL} width="100px" height="100px" style={imgStyle}/>
         </div>
         <div dir="horizontal" style={style2}>
             {renderLR && combined_lr != undefined &&
